f82e83b5bc25a73e518c20898be15682bc27a3b2
Kirito <kilito@pm.me>
2023-07-09 01:38:50 +0200

    Improved dialog scene

--- gameplay/dialog/style/DialogicLayout.gd
+++ gameplay/dialog/style/DialogicLayout.gd
@@ -12,38 +12,26 @@ func _ready() -> void:
 
 
 func on_character_joined(_info: Dictionary):
-	#print_debug("character joined")
-	
-	#if %TextMargin.:
-	#	return
-	
 	var tween = create_tween()
 	
 	# move text to the right
-	#tween.tween_property(%TextMargin, "theme_override_constants/margin_left", portrait_margin, animation_duration)
 	tween.tween_method(tween_margin, portrait_default_margin, portrait_margin, animation_duration)
 	
 	# slide name in
-	tween.parallel().tween_property(%NameContainer, "position:x", -8, animation_duration).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+	tween.parallel().tween_property(%NameContainer, "position:x", -40, animation_duration).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_CUBIC)
 	
 	# fade name in
 	tween.parallel().tween_property(%NameContainer, "modulate", Color(1.0, 1.0, 1.0, 1.0), animation_duration)
 
 
 func on_character_left(_info: Dictionary):
-	#print_debug("character left")
-	
-	#if %TextMargin.theme_override_constants.margin_left == portrait_default_margin:
-	#	return
-	
 	var tween = create_tween()
 	
 	# move text to the left
-	#tween.tween_property(%TextMargin, "theme_override_constants/margin_left", portrait_default_margin, animation_duration)
 	tween.tween_method(tween_margin, portrait_margin, portrait_default_margin, animation_duration)
 
 	# slide name in
-	tween.parallel().tween_property(%NameContainer, "position:x", -235, animation_duration).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_BACK)
+	tween.parallel().tween_property(%NameContainer, "position:x", -265, animation_duration).set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_BACK)
 	
 	# fade name in
 	tween.parallel().tween_property(%NameContainer, "modulate", Color(1.0, 1.0, 1.0, 0.0), animation_duration)
