82ff4daccbf77265b46236b5c0a1dd34647ec503
Kirito <kilito@pm.me>
2023-07-12 00:00:27 +0200

    Fix vision ability

--- characters/abilities/vision_ability.gd
+++ characters/abilities/vision_ability.gd
@@ -1,11 +1,8 @@
 extends Node2D
 
 
-const HINT_MARGIN: float = 20
+const HINT_MARGIN: int = 20
 const VIEWPORT_SIZE: Vector2 = Vector2(480, 270)
-
-@onready var player: Player = get_tree().get_first_node_in_group("player")
-@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 var target: Node2D
 
@@ -18,31 +15,39 @@ func _unhandled_input(event: InputEvent) -> void:
 		target = get_tree().get_first_node_in_group("vision_target")
 	
 	if target == null:
+		print_debug("vision ability found no target")
 		return
 	
-	sprite.play("default")
+	%Hint.play("splosh")
 
 
 func _physics_process(delta: float) -> void:
 	if target == null:
 		return
 	
-	var viewport_position = get_viewport_transform().origin * -1
+	#print("viewport_transform", get_viewport_transform())
+	#print("screen_transform", get_screen_transform())
+	#print("canvas_transform", get_canvas_transform())
+	#print("canvas_affine", get_canvas_transform().affine_inverse()) !
+	#print("global_transform", get_global_transform())
+	#print("global_w_canvas", get_global_transform_with_canvas())
+	
+	var canvas_position = get_canvas_transform().affine_inverse().origin
 	
 	var hint_position = target.position
-	# keep x on screen
-	if hint_position.x < viewport_position.x + HINT_MARGIN:
-		hint_position.x = viewport_position.x + HINT_MARGIN
 	
-	elif hint_position.x > viewport_position.x + VIEWPORT_SIZE.x - HINT_MARGIN:
-		hint_position.x = viewport_position + VIEWPORT_SIZE.x - HINT_MARGIN
+	# keep x on screen
+	if hint_position.x < canvas_position.x + HINT_MARGIN:
+		hint_position.x = canvas_position.x + HINT_MARGIN
+	
+	elif hint_position.x > canvas_position.x + VIEWPORT_SIZE.x - HINT_MARGIN:
+		hint_position.x = canvas_position + VIEWPORT_SIZE.x - HINT_MARGIN
 	
 	# keep y on screen
-	if hint_position.y < viewport_position.y + HINT_MARGIN:
-		hint_position.y = viewport_position.y + HINT_MARGIN
+	if hint_position.y < canvas_position.y + HINT_MARGIN:
+		hint_position.y = canvas_position.y + HINT_MARGIN
 	
-	elif hint_position.y > viewport_position.y + VIEWPORT_SIZE.y - HINT_MARGIN:
-		hint_position.y = viewport_position + VIEWPORT_SIZE.y - HINT_MARGIN
+	elif hint_position.y > canvas_position.y + VIEWPORT_SIZE.y - HINT_MARGIN:
+		hint_position.y = canvas_position.y + VIEWPORT_SIZE.y - HINT_MARGIN
 	
-	sprite.position = hint_position
-	
+	position = hint_position
