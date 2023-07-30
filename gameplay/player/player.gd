64b89d0a4bbe3e912a8f6a25c933b6e969e9135d
Kirito <kilito@pm.me>
2023-07-31 01:19:55 +0200

    Minor fixes

--- gameplay/player/player.gd
+++ gameplay/player/player.gd
@@ -53,16 +53,6 @@ func animation(delta: float) -> void:
 
 
 func movement(_delta: float) -> void:
-	var x_input := Input.get_axis("move_left", "move_right")
-	if x_input:
-		velocity.x = x_input * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-	
-	var y_input := Input.get_axis("move_up", "move_down")
-	if y_input:
-		velocity.y = y_input * speed
-	else:
-		velocity.y = move_toward(velocity.y, 0, speed)
-	
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = direction * speed
 	move_and_slide()
