c6a0848eb1de8899d5aba774708c895ebd59fe98
Kirito <kilito@pm.me>
2023-07-30 20:07:52 +0200

    Finish navigation layer

--- level/town/PathHolder.gd
+++ level/town/PathHolder.gd
@@ -1,7 +1,11 @@
 extends Node2D
 
+
+const move_speed := 4.0
+
+
 func _physics_process(delta):
-	const move_speed := 4.0
+	return
 	$Mann1Path/Mann1PathFollow.progress += move_speed * delta
 	$MetzgerPath/MetzgerPathFollow.progress += move_speed * delta
 	$AltFrauMannPath/AltFrauMannPathFollow.progress += move_speed * delta
